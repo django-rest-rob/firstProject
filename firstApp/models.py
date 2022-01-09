700456402e0ce357b1cd34dcc978c265001d312f
chamisarobert <chamisa.robert@gmail.com>
2022-01-09 23:09:26 +0200

    Added Employee model

--- firstApp/models.py
+++ firstApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    #function for string rep of Employee obj
+    def __str_(self):
+        return self.id + '|' + self.name + '|' + self.salary
--- firstApp/views.py
+++ firstApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def employeeView(request):
+    emp = {
+    'id': 123,
+    'name': 'Robert',
+    'salary': 10000
+    }
+
+    #JsonResponse will serialize the emp dict into JSON
+    return JsonResponse(emp)
