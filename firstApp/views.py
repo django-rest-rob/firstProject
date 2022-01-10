20a70e043464109468f1a20ea7beebc55ee5c1cd
chamisarobert <chamisa.robert@gmail.com>
2022-01-10 09:44:14 +0200

    DB access

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,13 +1,44 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from firstApp.models import Employee
 
 # Create your views here.
 def employeeView(request):
-    emp = {
+    '''emp = {
     'id': 123,
     'name': 'Robert',
     'salary': 10000
-    }
+    }'''
 
-    #JsonResponse will serialize the emp dict into JSON
-    return JsonResponse(emp)
+    # Get all Employees from db
+    data = Employee.objects.all()
+    # convert result QuerySet to a Dictionary usable by JsonResponse
+    response = {'employees': list(data.values('name', 'salary'))}
+
+    #JsonResponse will serialize the dict into JSON
+    return JsonResponse(response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
